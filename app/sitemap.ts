892c10dfb6324150bb904bf9d50bae76f96cd0d9
AbhishekSinghDev <singhabhishek7689@gmail.com>
2024-07-05 12:19:16 +0530

    fix: empty routes. google crawler

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,22 +8,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: `${BASE_URL}/`,
     },
     {
-      url: `${BASE_URL}/#section`,
-    },
-    {
-      url: `${BASE_URL}/#components`,
-    },
-    {
-      url: `${BASE_URL}/#clones`,
-    },
-    {
       url: `${BASE_URL}/section`,
     },
     {
-      url: `${BASE_URL}/components`,
+      url: `${BASE_URL}/component`,
     },
     {
-      url: `${BASE_URL}/clones`,
+      url: `${BASE_URL}/clone`,
     },
   ];
 }
